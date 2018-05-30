dcda040ac61587714b3963e62cc1656a92844eec
Bruno Mendoza <hola@brunomendoza.es>
2018-05-30 12:33:37 +0200

    Initial commit

---
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcMovie.Controllers
+{
+    public class HelloWorldController : Controller
+    {
+        // GET: HelloWorld
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HelloWorld/Welcome
+        public string Welcome(string name, int number)
+        {
+            return "Thank you for using 'Welcome' URL segment! You wrote " + name + " and " + number;
+        }
+    }
+}
